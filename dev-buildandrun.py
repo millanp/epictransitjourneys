5e1c2d4bdfae412ace1b3f6c26394ab2dd35e3c6
millanp <millan.philipose@gmail.com>
2018-08-02 21:56:22 -0700

    add comments to dev buildandrun

--- dev-buildandrun.py
+++ dev-buildandrun.py
@@ -1,2 +1,10 @@
 import os
-os.system('npm install && python manage.py collectstatic --no-input && heroku local')
+
+# Simulate Heroku Nodejs buildpack
+os.system('npm install')
+
+# Simulate Heroku Python buildpack (all pip dependencies are already installed)
+os.system('python manage.py collectstatic --no-input')
+
+# Run the Procfile locally
+os.system('heroku local')
